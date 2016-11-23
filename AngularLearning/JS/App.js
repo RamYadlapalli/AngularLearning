9e3f20fcdc76d65bb05340965af8a943d10087c6
RambabuY <rambabu.1990@hotmail.com>
2016-11-23 15:00:33 +0530

    Updated with filters

--- AngularLearning/JS/App.js
+++ AngularLearning/JS/App.js
@@ -2,34 +2,47 @@
 var app = angular.module("MyApp", ['ngSanitize']);
 
 app.controller("HomeController", function ($scope) {
-    $scope.Name = "TestApp";
+    $scope.Name = "Sessions List";
     $scope.Date = "11/22/2016";
     $scope.snippet = '<span style="color:red">Hi there</span>',
         $scope.boolVal = false,
+        $scope.buttonDisabled = true,
+        $scope.sortOrder = "Name",
+        $scope.resultsLimit =4,
     $scope.Sessions = [{
-        Name: "Session1",
+        Name: "Bbc",
         Type: "IP",
-        VoteCount: 0
+        VoteCount: 0,
+        Level: "Expert",
+        Duration: 4
     },
         {
             Name: "Session1",
             Type: "IP",
-            VoteCount:0
+            VoteCount: 0,
+            Level: "Beginner",
+            Duration: 2
         },
         {
-            Name: "Session5",
+            Name: "ASession5",
             Type: "IP",
-            VoteCount: 0
+            VoteCount: 0,
+            Level: "Intermediate",
+            Duration: 1
         },
         {
             Name: "Session3",
             Type: "IP",
-            VoteCount: 0
+            VoteCount: 0,
+            Level: "Beginner",
+            Duration: 5
         },
         {
             Name: "Session2",
             Type: "IP",
-            VoteCount: 0
+            VoteCount: 0,
+            Level: "Expert",
+            Duration: 1
         }
     ];
 
@@ -42,4 +55,10 @@ app.controller("HomeController", function ($scope) {
     };
 }
 
+   
 );
+app.filter("addHours", function () {
+    return function (Duration) {
+        return Duration + " Hours";
+    };
+});
